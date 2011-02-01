1f1272655961be4df64873f0064e44d1062b7455
stef <s@agilitik.com>
2011-02-02 00:23:49 +0100

    initial commit, initialize ror app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WwwAgilitikCom::Application.config.secret_token = 'bacd4c2c57d2b3db6da2c154ad5e01d01e2369ca2fec5bfffe85bfb00d15692a79429625a56de4990566f16d6e5eb9b295fa8754fe33e1e931d5962f7ec34cdb'
